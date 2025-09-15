b2b4552ec914942db75feeb40cd11da913870764
Dhvanitkanabar <dhvanit.kanabar.cg@gmail.com>
2025-09-15 12:15:46 +0530

    Pattern Question 1 to 20 added

---
+++ C_language/Pattern_questions/q20.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=6,k=1;
+    for(int i=1;i<=n;i++)
+    {
+       
+        for(int j=1;j<=i;j++)
+        {
+            printf("%c ",'A'+k++-1);
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
---
+++ C_language/Pattern_questions/q4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=4;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+    
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
+
+
